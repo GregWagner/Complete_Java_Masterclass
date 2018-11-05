6455d70e67c03965a9c54f79d6188e7bbc5a1927
GregWagner <theGreg101@gmail.com>
2018-11-05 11:29:28 -0600

    Finished Chapter 5

--- Section_05_Control_Flow_Statements/Challenges/NumberOfDaysInMonth.java
+++ Section_05_Control_Flow_Statements/Challenges/NumberOfDaysInMonth.java
@@ -1,10 +1,17 @@
 public class NumberOfDaysInMonth {
     public static void main(String[] args) {
-        System.out.println(isLeapYear(0));
-        System.out.println(isLeapYear(10000));
-        System.out.println(isLeapYear(1600));
-        System.out.println(isLeapYear(2017));
-        System.out.println(isLeapYear(2000));
+        System.out.println(isLeapYear(0) == false);
+        System.out.println(isLeapYear(10000) == false);
+        System.out.println(isLeapYear(-1600) == false);
+        System.out.println(isLeapYear(1600) == true);
+        System.out.println(isLeapYear(2017) == false);
+        System.out.println(isLeapYear(2000) == true);
+
+        System.out.println(getDaysInMonth(1, 2020) == 31);
+        System.out.println(getDaysInMonth(2, 2020) == 29);
+        System.out.println(getDaysInMonth(2, 2018) == 28);
+        System.out.println(getDaysInMonth(-1, 2020) == -1);
+        System.out.println(getDaysInMonth(1, -2020) == -1);
     }
 
     public static boolean isLeapYear(int year) {
@@ -15,5 +22,29 @@ public class NumberOfDaysInMonth {
             return true;
         }
         return false;
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+        if (month < 1 || month > 12 || year < 1 || year > 9999) {
+            return -1;
+        }
+        boolean leap = isLeapYear(year);
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 31;
+            case 2:
+                if (leap) {
+                    return 29;
+                }
+                return 28;
+            default:
+                return 30;
+        }
     }
 }
